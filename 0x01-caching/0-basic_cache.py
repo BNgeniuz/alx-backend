ad6c57b3e036abdb27de6579541ad94386ab39a4
Godwin Bruce <godwin.bruce@gmail.com>
2024-09-04 01:46:36 +0000

    basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,18 +4,18 @@ Basic dictionary that inherits from a base
 caching module
 """
 
-BaseCaching = __import__("base_caching").BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
     """defines 2 methods that inherits from a dict"""
     def put(self, key, item):
         """adds item to a dict"""
-        if key and item:
+        if not (key is None or item is None):
             self.cache_data[key] = item
 
     def get(self, key):
         """returns a value linked to key"""
-        if key is None or key not in self.cache_data.keys():
+        if key is None or not (key in self.cache_data):
             return None
-        return self.cache_data.get(key)
+        return self.cache_data[key]
